c48c8a41a6ab419b28876fabe9a919e938086dfd
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-02-07 05:09:28 +0000

    [Feature] function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module using the Reddit API
+"""
+import requests
+from sys import argv
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    try:
+        if subreddit is None or type(subreddit) is not str:
+            print(None)
+
+        url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+        request = requests.get(url, headers={"User_agent": "Nicolás"})
+
+        if (request.status_code != 200):
+            print(None)
+
+        data = request.json().get("data")
+        children = data.get("children")
+
+        for counter in children:
+            print(counter.get("data").get("title"))
+
+    except Exception as err:
+        print(None)
