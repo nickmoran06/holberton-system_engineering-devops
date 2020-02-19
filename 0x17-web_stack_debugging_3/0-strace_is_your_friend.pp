9eb8a81617b29c5db976dd300f5fb46d8f07db82
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-02-19 22:01:26 +0000

    [Fix] puppet-lint and path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # typo 500 error, to fix it is changing php instead phpp
-exec { 'php_instead_phpp':                         
-path     => 'usr/bin/:/bin/',                         
-command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",                         
-provider => 'shell',                       
+exec { 'php_instead_phpp':
+path     => '/bin/',
+command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+provider => 'shell',
 }
