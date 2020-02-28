4f4fd11189d4337830df941e80aa2804a138c195
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-02-28 04:49:15 +0000

    [Fix] order and number of exec

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,6 @@
 # Changing soft instead hard and viceversa
 
-exec {'soft':
-  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
-}
-
-exec {'hard':
-  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
-  before   => Exec['soft'],
+exec {'soft_hard_feature':
+  command => "/usr/bin/sed -i 's/holberton hard nofile 5/holberton hard nofile 50' /etc/security/limits.conf;\
+  /usr/bin/sed -i 's/holberton soft nofile 4/holberton soft nofile 40' /etc/security/limits.conf"
 }
