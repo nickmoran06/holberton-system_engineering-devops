45d0f78100f912d3724baf7791241ac601ff81bc
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-02-28 04:57:54 +0000

    [Fix] quote extra

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increases the open file limit in default file of nginx
 
 exec {'open_limit':
+  command  => 'sudo sed -i 's/15/4096' /etc/default/nginx && sudo service nginx restart',
   provider => shell,
-  command  => 'sudo sed -i 's/15/4096' /etc/default/nginx' && sudo service nginx restart,
 }
