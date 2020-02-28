0402bdb5d752363f9bbcff0ec26117093282371b
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-02-28 04:59:31 +0000

    [Fix] order and syntax

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,4 @@
 # Increases the open file limit in default file of nginx
-
-exec {'open_limit':
-  command  => 'sudo sed -i 's/15/4096' /etc/default/nginx && sudo service nginx restart',
+  command  => "sudo sed -i 's/15/1000' /etc/default/nginx && sudo service nginx restart",
   provider => shell,
 }
